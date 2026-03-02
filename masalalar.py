423f470727a9a264197f1d2c0b91f5624f124495
asm1127 <marjona1marjona2@gmail.com>
2026-03-02 18:04:45 +0500

    yechim

---
+++ masalalar.py
@@ -0,0 +1,39 @@
+# #1
+# from dataclasses import dataclass
+# from email.iterators import body_line_iterator
+#
+#
+# @dataclass
+# class Tortburchak:
+#     eni: int
+#     boyi: int
+#
+#     def hisobla(self):
+#         return self.eni * self.boyi
+#
+# shakl = Tortburchak(15, 6)
+#
+# print(shakl.hisobla())
+
+# #2
+# from dataclasses import dataclass
+#
+# @dataclass
+# class Doira:
+#     radius: float
+#
+# shakl = Doira(5)
+# print(shakl.radius * shakl.radius * 3.14)
+
+
+# #3
+# from dataclasses import dataclass
+#
+# @dataclass
+# class Talaba:
+#     ism: str
+#     baho: int
+#
+# talaba_bahosi = Talaba("Shaxlo", 5)
+#
+# print(f"{talaba_bahosi.ism}ning baxosi: {talaba_bahosi.baho}")
