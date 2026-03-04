326e8797cc7f92e89d635c8ccd728ad52ba7f2e8
asm1127 <marjona1marjona2@gmail.com>
2026-03-04 17:08:15 +0500

    yechim

---
+++ masalalar2.py
@@ -0,0 +1,119 @@
+# #1
+# from dataclasses import dataclass
+#
+# @dataclass
+# class Talaba:
+#     ism: str
+#     yosh: int
+#     kurs: int
+#
+# Doniyor = Talaba('Doniyor', 20, 3)
+# print(Doniyor)
+
+# #2
+# from dataclasses import dataclass
+#
+# @dataclass
+# class Kitob:
+#     nomi: str
+#     muallif: str
+#     sahifa_soni: int
+#
+# Binafsha_shulasi = Kitob('Binafsha shulasi', 'Usoma Muslim', 383)
+# print(Binafsha_shulasi)
+
+# #3
+# from dataclasses import dataclass
+#
+# @dataclass
+# class Telefon:
+#     brand: str
+#     model: str
+#     narx: int
+#
+# tel = Telefon('Samsung', 'Samsung S26', 12_200_000)
+# print(tel)
+
+# #4
+# from dataclasses import dataclass
+#
+# @dataclass
+# class Avtomobil:
+#     marka: str
+#     rang: str
+#     yil: int
+#
+# malibu = Avtomobil('Malibu', 'qora', 2025)
+# print(malibu)
+
+# #5
+# from dataclasses import dataclass
+#
+# @dataclass
+# class Shahar:
+#     nomi: str
+#     davlat: str
+#     aholi: int
+#
+# Tokyo = Shahar('Tokyo', 'Yaponiya', 14_000_000)
+# print(Tokyo)
+
+# #6
+# from dataclasses import dataclass
+#
+# @dataclass
+# class Kompyuter:
+#     protsessor: str
+#     ram: int
+#     xotira: int
+#
+# komp = Kompyuter('Intel Xeon', 16, 256)
+# print(komp)
+
+# #7
+# from dataclasses import dataclass
+#
+# @dataclass
+# class Film:
+#     nomi: str
+#     janr: str
+#     yil: int
+#
+# kino = Film('Megan', 'Horror', 2022)
+# print(kino)
+
+# #8
+# from dataclasses import dataclass
+#
+# @dataclass
+# class Hisob:
+#     ism: str
+#     hisob_raqam: int
+#     balans: int
+#
+# hisob = Hisob('Davlat', 235, 2_000_000)
+# print(hisob)
+
+# #9
+# from dataclasses import dataclass
+#
+# @dataclass
+# class Oqituvchi:
+#     ism: str
+#     fan: str
+#     tajriba_yili: int
+#
+# muallim = Oqituvchi('Dilorom', 'Ona tili', 10)
+# print(muallim)
+
+# #10
+# from dataclasses import dataclass
+#
+# @dataclass
+# class Mahsulot:
+#     nomi: str
+#     narx: int
+#     miqdor: int
+#
+# narsa = Mahsulot('Sut', 15_000, 6)
+# print(narsa)
